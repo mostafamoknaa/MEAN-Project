290901798f4d6bfed9ecfcf34984cebd5dfc3190
Mostafa <mostafamokna78@gmail.com>
2025-03-07 23:49:40 +0200

    CRUD for user

--- Controller/user_controller.js
+++ Controller/user_controller.js
@@ -3,6 +3,10 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import sendEmail from '../Email/email.js'
 
+
+
+
+
 const signup = async(req, res) => {
     try {
 
@@ -63,9 +67,241 @@ const verifyEmail = (req, res) => {
 
 }
 
+const addToWishlist = async(req, res) => {
+    try {
+        const userId = req.user.id;
+        const { wishlist } = req.body;
+
+        if (!wishlist || !Array.isArray(wishlist)) {
+            return res.status(400).json({ message: "Invalid wishlist data" });
+        }
 
 
+        const updatedUser = await userModel.findByIdAndUpdate(
+            userId, { $addToSet: { wishlist: { $each: wishlist } } }, { new: true }
+        ).populate("wishlist");
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({
+            message: "Wishlist updated successfully",
+            wishlist: updatedUser.wishlist
+        });
+    } catch (error) {
+        console.error("Error updating wishlist:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+const deleteProductFromWishlist = async(req, res) => {
+    try {
+        const userId = req.user.id;
+        const productId = req.params.id;
+
+        const user = await userModel.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
 
 
+        if (!user.wishlist.includes(productId)) {
+            return res.status(400).json({ message: "Product not found in wishlist" });
+        }
 
-export { signup, signin, verifyEmail };
+
+        user.wishlist = user.wishlist.filter(item => item.toString() !== productId);
+        await user.save();
+
+        res.status(200).json({
+            message: "Product removed from wishlist successfully",
+            wishlist: user.wishlist
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+const getUserinfo = async(req, res) => {
+
+    const userId = req.user.id;
+    try {
+        const user = await userModel.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({
+            message: "User information retrieved successfully",
+            user: user
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+const updateUserinfo = async(req, res) => {
+    const userId = req.user.id;
+    const { name, email, phone } = req.body;
+    try {
+        const user = await userModel.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        user.name = name;
+        user.email = email;
+        user.phone = phone;
+        await user.save();
+        res.status(200).json({
+            message: "User information updated successfully",
+            user: user
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+const deleteUserinfo = async(req, res) => {
+    const userId = req.user.id;
+    try {
+        const user = await userModel.findByIdAndDelete(userId);
+        if (!user) {
+
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({
+            message: "User deleted successfully",
+
+            user: user
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+const getAllUsers = async(req, res) => {
+    try {
+
+        const userId = req.user.id;
+
+        const userrole = await userModel.findById(userId);
+
+        if (userrole.role !== "admin") {
+            return res.status(403).json({ message: "You are not admin" });
+        } else {
+
+            const users = await userModel.find();
+            res.json(users);
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const getUserById = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await userModel.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const createUser = async(req, res) => {
+
+    try {
+
+        const userId = req.user.id;
+
+        const userrole = await userModel.findById(userId);
+
+        if (userrole.role !== "admin") {
+            return res.status(403).json({ message: "You are not admin" });
+        } else {
+
+
+            let foundedEmail = await userModel.findOne({ email: req.body.email });
+
+            if (foundedEmail) {
+                return res.status(400).send({ message: "Email already exists" });
+            }
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+            req.body.isconfermed = true;
+            let user = new userModel(req.body);
+
+            await user.save();
+
+            res.status(201).send({ message: "User registered successfully" });
+        }
+    } catch (e) {
+        res.status(500).json({ message: e.message });
+    }
+}
+
+const updateUser = async(req, res) => {
+    try {
+
+
+        const userId = req.user.id;
+
+        const userrole = await userModel.findById(userId);
+
+        if (userrole.role !== "admin") {
+            return res.status(403).json({ message: "You are not admin" });
+        } else {
+            const id = req.params.id;
+            const user = await userModel.findById(id);
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+            const updatedUser = await userModel.findByIdAndUpdate(id, req.body, { new: true });
+            res.json(updatedUser);
+
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const deleteUser = async(req, res) => {
+    try {
+
+        const userId = req.user.id;
+
+        const userrole = await userModel.findById(userId);
+
+        if (userrole.role !== "admin") {
+            return res.status(403).json({ message: "You are not admin" });
+        } else {
+            const id = req.params.id;
+            const user = await userModel.findById(id);
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+            await userModel.findByIdAndDelete(id);
+            res.json({ message: "User deleted successfully" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export {
+    signup,
+    signin,
+    verifyEmail,
+    addToWishlist,
+    deleteProductFromWishlist,
+    getUserinfo,
+    deleteUserinfo,
+    updateUserinfo,
+    deleteUser,
+    updateUser,
+    getAllUsers,
+    getUserById,
+    createUser
+};
--- Route/user_route.js
+++ Route/user_route.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { authMiddleware } from "../Middleware/auth_token.js"
 
-import { signup, signin, verifyEmail } from "../Controller/user_controller.js"
+import { signup, signin, verifyEmail, updateUser, deleteUser, createUser, getUserById, getAllUsers, addToWishlist, deleteProductFromWishlist, getUserinfo, updateUserinfo, deleteUserinfo } from "../Controller/user_controller.js"
 
 
 
@@ -8,7 +9,22 @@ import { signup, signin, verifyEmail } from "../Controller/user_controller.js"
 export const route = express.Router();
 
 route.post('/signup', signup);
-
 route.post('/login', signin);
-
 route.get("/verify/:email", verifyEmail)
+
+//Admin
+
+route.get('/getuserinfo', authMiddleware, getUserinfo);
+route.put('/updateuserinfo', authMiddleware, updateUserinfo);
+route.delete('/deleteuserinfo', authMiddleware, deleteUserinfo);
+
+route.get('/users', authMiddleware, getAllUsers);
+route.get('/users/:id', getUserById);
+route.post('/users', authMiddleware, createUser);
+route.put('/users/:id', authMiddleware, updateUser);
+route.delete('/users/:id', authMiddleware, deleteUser);
+
+//whishlist
+
+route.post('/addtowishlist', authMiddleware, addToWishlist);
+route.delete('/deletefromwishlist/:id', authMiddleware, deleteProductFromWishlist);
